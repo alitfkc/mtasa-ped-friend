09cf1f5a3f96ad0e31b92212c71a581c7548be97
Alparslan Ali TÜFEKÇİ <103515832+alitfkc@users.noreply.github.com>
2023-01-12 22:50:12 +0300

    Update client.lua

--- pedfriend/client.lua
+++ pedfriend/client.lua
@@ -26,7 +26,7 @@ addEvent("sent_ped_meta",true)
 addEventHandler("sent_ped_meta",localPlayer,create_ped_event)
 
 function talk_ped(cmd,...)
-    local msg = string.lower(...)
+    local msg = string.lower(table.concat({...}," "))
     for k,v in pairs(messages) do 
         if string.lower(k) == msg then 
             if isTimer(timer) then 
