a25697c6b85e3476672a839c181b2a9e85212539
Alparslan Ali TÜFEKÇİ <103515832+alitfkc@users.noreply.github.com>
2023-01-12 20:34:27 +0300

    Add files via upload

---
+++ pedfriend_voicemode/settings.lua
@@ -0,0 +1,9 @@
+skin_id = 31
+command = "gr"
+talk_cmd ="gspeak"
+voice_lang = "ja"
+messages = {
+    ["alo"] = "yamate kudasai",
+    ["how re u"] = "I m fine what about you",
+    ["car_join"] = "ur car very sweeet",
+}
