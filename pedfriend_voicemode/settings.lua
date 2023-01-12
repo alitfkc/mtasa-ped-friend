15724f2d922722544f7c16d377d620f695f40785
Alparslan Ali TÜFEKÇİ <103515832+alitfkc@users.noreply.github.com>
2023-01-12 22:48:50 +0300

    Delete pedfriend_voicemode directory

--- pedfriend_voicemode/settings.lua
+++
@@ -1,9 +0,0 @@
-skin_id = 31
-command = "gr"
-talk_cmd ="gspeak"
-voice_lang = "ja"
-messages = {
-    ["alo"] = "yamate kudasai",
-    ["how re u"] = "I m fine what about you",
-    ["car_join"] = "ur car very sweeet",
-}
